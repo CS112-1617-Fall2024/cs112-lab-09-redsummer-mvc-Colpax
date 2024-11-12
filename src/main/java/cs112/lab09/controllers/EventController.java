02dd86a660444088ed01bd25b05e7839ae6a766a
Colpax <Etonespravedlivo@gmail.com>
2024-11-12 13:11:23 -0800

    2'nd commit

--- src/main/java/cs112/lab09/controllers/EventController.java
+++ src/main/java/cs112/lab09/controllers/EventController.java
@@ -5,10 +5,14 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+
+import java.awt.*;
+import java.net.URI;
 
 public class EventController {
 
@@ -46,8 +50,24 @@ public class EventController {
     private Label revisedDescriptionLabel;
 
     @FXML
+    private Hyperlink sourceLink;
+
+    @FXML
     protected void onCloseButtonClicked(ActionEvent actionEvent){
         Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
         stage.close();
+    }
+
+    @FXML
+    protected void onSourceClick(){
+        if(Desktop.isDesktopSupported()){
+            try {
+                Desktop.getDesktop().browse(new URI("https://youtu.be/dQw4w9WgXcQ"));
+            } catch (Exception e) {
+                System.out.println("Something wrong");
+            }
+        } else{
+            System.out.println("Desktop is not supported");
+        }
     }
 }
